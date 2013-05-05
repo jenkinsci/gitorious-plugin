8bd392cedee8dbb13269cbe87402d26433e37511
Guy Rozendorn <guy@rzn.co.il>
2013-05-05 09:42:36 +0300

    HOSTDEV-711 passing specific branch to GitStatus.doNotifyCommit

--- src/main/java/org/jenkinsci/plugins/gitorious/GitoriousStatus.java
+++ src/main/java/org/jenkinsci/plugins/gitorious/GitoriousStatus.java
@@ -70,6 +70,7 @@ public class GitoriousStatus implements UnprotectedRootAction {
         // HTTP clone access for gitorious is done by adding "git." to the front of the hostname,
         // but the payload URL doesn't come that way. So I added a check in case someone is using HTTP access in jenkins.
         String url2 = jsonObject.getJSONObject("repository").getString("url").replace("https://", "git://");
-        return gitStatus.doNotifyCommit(url2 + ".git", null);
+        String branch = jsonObject.getString("ref");
+        return gitStatus.doNotifyCommit(url2 + ".git", branch);
     }
 }
