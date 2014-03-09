f589163307425e9221bdd88c4558a56706e60e89
Guy Rozendorn <guy@rzn.co.il>
2014-03-09 17:03:23 +0200

    using new GitStatus APIs

--- src/main/java/org/jenkinsci/plugins/gitorious/GitoriousStatus.java
+++ src/main/java/org/jenkinsci/plugins/gitorious/GitoriousStatus.java
@@ -71,6 +71,7 @@ public class GitoriousStatus implements UnprotectedRootAction {
         // but the payload URL doesn't come that way. So I added a check in case someone is using HTTP access in jenkins.
         String url2 = jsonObject.getJSONObject("repository").getString("url").replace("https://", "git://");
         String branch = jsonObject.getString("ref");
-        return gitStatus.doNotifyCommit(url2 + ".git", branch);
+        String sha1 = jsonObject.getString("after");
+        return gitStatus.doNotifyCommit(url2 + ".git", branch, sha1);
     }
 }
